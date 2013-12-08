88fb3219ee0c8be3b114ac1e47ae2b4feb2715e8
Neal Thomison <bang@bangtree.com>
2013-12-08 12:41:52 -0500

    Workable configuration.
    This setup works and I'm getting things into a stable state before
    altering my config.

--- init.el
+++ init.el
@@ -5,6 +5,11 @@
 ;; This is the first thing to get loaded.
 ;;
 
+;; add my org-mode early so it will be used for everything
+
+(add-to-list 'load-path "~/.emacs.d/lisp/org-mode/lisp")
+(add-to-list 'load-path "~/.emacs.d/lisp/org-mode/contrib/lisp")
+
 ;; load the starter kit from the `after-init-hook' so all packages are loaded
 (add-hook 'after-init-hook
  `(lambda ()
